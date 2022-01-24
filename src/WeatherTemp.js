9eaa55a68bbdb6cb75ca6171b39c7c58f91819a6
natkaulaP <89267067+natkaulaP@users.noreply.github.com>
2022-01-24 17:11:08 +0100

    Update WeatherTemp.js

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function WeatherTemp(props) {
+const [unit, setUnit]=useState("celsius")
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  setUnit("fahrenheit");
+}
+function convertToCelsius(event){
+   event.preventDefault();
+   setUnit("celsius");
+}
+function fahrenheit(){
+  return (props.celsius * 9) / 5 + 32;
+}
+if(unit==="celsius"){
   return (
     <div className="WeatherTemp">
       <span className="temp">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
+      <span className="unit">
+        °C|{" "}
+        <a href="/" onClick={convertToFahrenheit}>
+          °F
+        </a>
+      </span>
     </div>
-  );
+  );}else{
+    return (
+      <div className="WeatherTemp">
+        <span className="temp">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToCelsius}>
+            {" "}
+            °C{" "}
+          </a>
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
