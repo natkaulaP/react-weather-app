536d6eaf840868a460e1c7563c856dea4540a221
natkaulaP <89267067+natkaulaP@users.noreply.github.com>
2022-01-24 16:27:26 +0100

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -21,7 +21,7 @@ export default function FormattedDate(props) {
     minutes = `0${minutes}`;
   }
   return (
-    <div>
+    <div className="data-and-time">
       {day} {hours}:{minutes}
     </div>
   );
